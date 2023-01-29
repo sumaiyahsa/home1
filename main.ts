e2f5172dea0228d9954e0cf33e6c0d39e7ffbfc1
sumaiyahsa <123850466+sumaiyahsa@users.noreply.github.com>
2023-01-29 10:00:07 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("สวยยย")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("เนอะ")
+})
+radio.setGroup(191)
